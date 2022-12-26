ee64174792a4d8ecad3c3513a07c10bd17939724
Ouzar Merouane <ouzar.merouane@gmail.com>
2022-12-26 01:54:12 -0500

    i added the responsivness for the home page

--- components/LoginCard.tsx
+++ components/LoginCard.tsx
@@ -4,16 +4,16 @@ import Image from "next/image"
 const LoginCard = () => {
   return (
   
-        <div className="   w-full bg-white justify-around items-center grid grid-cols-2">
-            <div className="bg-transparent flex flex-col gap-10  items-center">
+        <div className="   w-full bg-white justify-around items-center grid grid-cols-1 md:grid-cols-2">
+            <div className="hidden bg-transparent md:flex flex-col gap-10  items-center">
                 <div>
                <Image  className="object-fit-cover"
                src='/twitter-Home.png' width={400} height={400} alt='logo twitter'/>
               </div>
-              <h4 className="text-xl font-semibold border-b-2 border-slate-300 w-full text-center pb-2">Twitter Clone</h4>
+              <h4 className="text-xl font-semibold lg:border-b-2 border-slate-300 w-full text-center pb-2">Twitter Clone</h4>
               
              
-               <div className="flex mt-4 mx-2 py-4 px-2 justify-between w-3/5  rounded-xl  text-gray-200 gap-2  text-sm">
+               <div className="hidden lg:flex mt-4 mx-2 py-4 px-2 justify-between w-3/5  rounded-xl  text-gray-200 gap-2  text-sm">
                     <p className="bg-white border-slate-800 border-2 text-slate-800 rounded-xl p-2 ">Login with G</p>
                     <p className="bg-white border-slate-800 border-2 text-slate-800 rounded-xl p-2 ">Login with F</p>
                     <p className="bg-white border-slate-800 border-2 text-slate-800 rounded-xl p-2 ">Login with A</p>
