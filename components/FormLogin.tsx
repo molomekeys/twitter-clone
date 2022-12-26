b32370a80587d242adb9fdcb708bf85321ced74c
Ouzar Merouane <ouzar.merouane@gmail.com>
2022-12-26 01:30:53 -0500

    added the style for the login page and i added the useRef for the form

--- components/FormLogin.tsx
+++ components/FormLogin.tsx
@@ -1,14 +1,27 @@
+import { useState,useRef } from "react"
 export const FormLogin = () => {
+    const [loginInput,setLoginInput]=useState({email:'',passeword:''})
+    const loginRef=useRef(null);
+    const passeworldRef=useRef(null);
+    const handleLogin=(e:any)=>{
+        e.preventDefault();
+        console.log(loginRef.current.value);
+        console.log(passeworldRef.current.value);
+    }
   return (
     <>
     
-    <form className=" flex flex-col gap-5 w-80 border-2 border-slate-300 p-2">
+    <form className=" flex flex-col gap-5 w-4/5 border-2 border-slate-300 p-5">
     <label>Email :</label>
-        <input placeholder="mail" className="p-2 border-2 border-slate-300"/>
+        <input placeholder="mail" ref={loginRef}
+        
+        className="p-2 border-2 border-slate-300"/>
         <label>Passeword :</label>
-        <input placeholder="password" className="p-2 border-2 border-slate-300"/>
+        <input ref={passeworldRef}
+        placeholder="password" className="p-2 border-2 border-slate-300"/>
        
-       <button className="m-10 bg-blue-500 text-white py-2 ">Login In </button>
+       <button onClick={handleLogin}
+       className="m-10 bg-blue-500 text-white py-2 ">Login In </button>
     </form>
     </>
   )
--- components/LoginCard.tsx
+++ components/LoginCard.tsx
@@ -4,14 +4,16 @@ import Image from "next/image"
 const LoginCard = () => {
   return (
   
-        <div className=" flex  w-full bg-white justify-around items-center">
-            <div className="bg-transparent flex flex-col gap-20">
+        <div className="   w-full bg-white justify-around items-center grid grid-cols-2">
+            <div className="bg-transparent flex flex-col gap-10 bg-gray-400 items-center">
                 <div>
                <Image  className="object-fit-cover"
                src='/twitter-Home.png' width={400} height={400} alt='logo twitter'/>
               </div>
-               <hr></hr>
-               <div className="flex mt-4 mx-2 py-4 px-2 justify-between  rounded-xl  text-gray-700 gap-2">
+              <h4 className="text-xl font-semibold border-b-2 border-slate-600 w-full text-center">Twitter Clone</h4>
+              
+             
+               <div className="flex mt-4 mx-2 py-4 px-2 justify-between w-3/5  rounded-xl  text-gray-700 gap-2 ">
                     <p className="bg-white border-slate-800 border-2 text-slate-800 rounded-xl p-2 ">Login with G</p>
                     <p className="bg-white border-slate-800 border-2 text-slate-800 rounded-xl p-2 ">Login with F</p>
                     <p className="bg-white border-slate-800 border-2 text-slate-800 rounded-xl p-2 ">Login with A</p>
@@ -19,8 +21,8 @@ const LoginCard = () => {
                </div>
                
            
-            <div className="flex flex-col text-center gap-20">
-                    <h3 className="text-3xl">Welcome again</h3>
+            <div className="flex flex-col  gap-20 bg-gray-50 w-full h-full items-center justify-center">
+                    <h3 className="text-3xl text-black">Welcome again</h3>
                     <FormLogin/>
             </div>
         </div>
